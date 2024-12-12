dabb62b4a6b8d9841f447bb113012afa5bfe770e
collegesoul <sotonyeharry37@gmail.com>
2024-12-12 18:30:09 +0200

    Added test for CategoryRepository

--- backend/MyFashionTrunk/src/main/java/com/example/myFashionTrunk/category/CategoryRepository.java
+++ backend/MyFashionTrunk/src/main/java/com/example/myFashionTrunk/category/CategoryRepository.java
@@ -1,5 +1,7 @@
 package com.example.myFashionTrunk.category;
 
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -12,6 +14,8 @@ public interface CategoryRepository extends ListCrudRepository<Category, Integer
      * Deletes all categories associated to a specific user
      * @param userId the id of the user
      * **/
+    @Modifying
+    @Transactional
     @Query("DELETE FROM Category c WHERE c.user.id = :userId ")
     void deleteAllByUserId(@Param("userId") Integer userId);
 }
---
+++ backend/MyFashionTrunk/src/test/java/com/example/myFashionTrunk/category/CategoryRepositoryTest.java
@@ -0,0 +1,57 @@
+package com.example.myFashionTrunk.category;
+
+import com.example.myFashionTrunk.user.User;
+import com.example.myFashionTrunk.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class CategoryRepositoryTest {
+    @Autowired
+    private CategoryRepository categoryRepo;
+    @Autowired
+    private UserRepository userRepo;
+
+    @BeforeEach
+    public void setUp() {
+        User user = User.builder()
+                .name("John")
+                .surname("Doe")
+                .email("john@example.com")
+                .password("Password5")
+                .build();
+        userRepo.save(user);
+
+        Category category1 = Category.builder()
+                .user(user)
+                .name("Category 1")
+                .type("Allowed")
+                .build();
+
+        Category category2 = Category.builder()
+                .user(user)
+                .name("Category 2")
+                .type("Prohibited")
+                .build();
+
+        categoryRepo.save(category1);
+        categoryRepo.save(category2);
+    }
+
+    @Test
+    public void whenDeleteAllByUserId_thenAllAssociatedCategoriesAreDeleted() {
+        Integer userId = 1;
+        categoryRepo.deleteAllByUserId(userId);
+        List<Category> categories = categoryRepo.findAll()
+                .stream().filter(c -> c.getUser().getId().equals(userId)).toList();
+        assertThat(categories).hasSize(0);
+    }
+}
